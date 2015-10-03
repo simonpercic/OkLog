c62918ceec45bb49c22607761392c3661cfcb8a3
Simon Perčič <simon@percic.si>
2015-10-03 20:44:07 +0200

    Renamed remote url base

--- oklog/src/main/java/com/github/simonpercic/oklog/utils/Constants.java
+++ oklog/src/main/java/com/github/simonpercic/oklog/utils/Constants.java
@@ -13,6 +13,6 @@ public final class Constants {
 
     public static final String LOG_TAG = "OKLOG";
     public static final String LOG_URL_BASE_LOCAL = "http://localhost:8080";
-    public static final String LOG_URL_BASE_REMOTE = "http://jsonecho.herokuapp.com";
+    public static final String LOG_URL_BASE_REMOTE = "http://responseecho.herokuapp.com";
     public static final String LOG_URL_ECHO_RESPONSE_PATH = "/re/";
 }

3aa52fac7e98042bd32b0e83fc5a8f9b550b0749
Simon Perčič <simon@percic.si>
2015-10-03 11:15:36 +0200

    Changed url base

--- oklog/src/main/java/com/github/simonpercic/oklog/utils/Constants.java
+++ oklog/src/main/java/com/github/simonpercic/oklog/utils/Constants.java
@@ -10,6 +10,6 @@ public final class Constants {
 
     public static final String LOG_TAG = "OKLOG";
     public static final String LOG_URL_BASE_LOCAL = "http://localhost:8080";
-    public static final String LOG_URL_BASE_REMOTE = "http://oklog.herokuapp.com";
+    public static final String LOG_URL_BASE_REMOTE = "http://jsonecho.herokuapp.com";
     public static final String LOG_URL_ECHO_RESPONSE_PATH = "/re/";
 }

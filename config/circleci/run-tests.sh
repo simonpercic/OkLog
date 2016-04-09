9bcb73b4e05cc75933f474cc952cf1ff3ed88716
Simon Perčič <simon@percic.si>
2016-04-09 17:40:39 +0200

    CircleCI emulator background

--- config/circleci/run-tests.sh
+++ config/circleci/run-tests.sh
@@ -5,7 +5,7 @@ then
     ./gradlew check
 elif [ $CIRCLE_NODE_INDEX -eq 1 ]
 then
-    emulator -avd circleci-android22 -no-audio -no-window
+    emulator -avd circleci-android22 -no-audio -no-window &
     circle-android wait-for-boot
     ./gradlew connectedCheck
 fi

b6b11827c692c5022e1aad25ddde381a619b7742
Simon Perčič <simon@percic.si>
2017-06-05 21:48:27 +0200

    Added POST, PUT, DELETE, HEADER test with all params

--- oklog3/src/androidTest/java/com/github/simonpercic/oklog3/OkLogInterceptorTest.kt
+++ oklog3/src/androidTest/java/com/github/simonpercic/oklog3/OkLogInterceptorTest.kt
@@ -117,6 +117,36 @@ class OkLogInterceptorTest {
     }
 
     @Test
+    fun testPostAll() {
+        val (client, urlCaptor) = createClientArgCaptor { it.withAllLogData() }
+
+        val url = server.url("/watched")
+
+        val requestBody = createRequestBody("{\"show\":5}")
+
+        val request = request(url)
+            .post(requestBody)
+            .addHeader(REQUEST_HEADER_NAME, REQUEST_HEADER_VALUE)
+            .build()
+
+        val mockResponse = MockResponse()
+            .setBody("{\"show\":{\"id\":5,\"name\":\"True Detective\",\"runtime\":60,\"network\":{\"id\":8,\"name\"" +
+                ":\"HBO\"}},\"watched_count\":107}")
+            .addHeader(CONTENT_TYPE, APPLICATION_JSON)
+            .addHeader(RESPONSE_HEADER_NAME, RESPONSE_HEADER_VALUE)
+
+        newCall(client, request, mockResponse)
+
+        assertEquals(
+            "http://responseecho-simonpercic.rhcloud.com/v1/r/H4sIAAAAAAAAAKtWKs7IL1eyqlbKTFGyMtVRykvMTVWyUgopKk1VcE" +
+                "ktSU0uySxLVdJRKirNK8kESZkZABWllpTnF2XDtFnAtXk4-SvV1uoolSeWJGekpsQn5wO1KVkZGpjXAgANYYQRagAAAA==?qb=H" +
+                "4sIAAAAAAAAAKtWKs7IL1eyMq0FAFlhsMYKAAAA&d=H4sIAAAAAAAAAONiCfAPDhGSzSgpKbDS18_JT07MycgvLrEyNTAw0C9PL" +
+                "EnOSE2R4gBJ6xvqGSpJlqRWlOgX5CRm5lkrJGckFhWnltiWlqTpWmhwGSlw8QalFnqkJqakFvkl5qYK8QO5rhWJuQU5qWGJOaWp" +
+                "FowODB4nGIOY_L0TsrIUuHic8_NKUvNKdEMqC1KFBBILCnIykxNLMvPz9LOK8_OywAYWoxpYjGxgAWNFVhMjftcDAPvg-mjjAAAA",
+            urlCaptor.value)
+    }
+
+    @Test
     fun testPutDefault() {
         val (client, urlCaptor) = createClientArgCaptor()
 
@@ -139,6 +169,37 @@ class OkLogInterceptorTest {
     }
 
     @Test
+    fun testPutAll() {
+        val (client, urlCaptor) = createClientArgCaptor { it.withAllLogData() }
+
+        val url = server.url("/show")
+
+        val requestBody = createRequestBody("{\"name\":\"True Detective\",\"network\":{\"id\":8},\"runtime\":60}")
+
+        val request = request(url)
+            .put(requestBody)
+            .addHeader(REQUEST_HEADER_NAME, REQUEST_HEADER_VALUE)
+            .build()
+
+        val mockResponse = MockResponse()
+            .setBody("{\"id\":5,\"name\":\"True Detective\",\"runtime\":60,\"network\":{\"id\":8,\"name\":\"HBO\"}}")
+            .setStatus("HTTP/1.1 %d %s".format(201, "Created"))
+            .addHeader(CONTENT_TYPE, APPLICATION_JSON)
+            .addHeader(RESPONSE_HEADER_NAME, RESPONSE_HEADER_VALUE)
+
+        newCall(client, request, mockResponse)
+
+        assertEquals(
+            "http://responseecho-simonpercic.rhcloud.com/v1/r/H4sIAAAAAAAAAKtWykxRsjLVUcpLzE1VslIKKSpNVXBJLUlNLsksS1" +
+                "XSUSoqzSvJBEmZGQAVpZaU5xdlK1lVg7VZwLV5OPkr1dYCAB5WJYFNAAAA?qb=H4sIAAAAAAAAAKtWykvMTVWyUgopKk1VcEktS" +
+                "U0uySxLVdJRykstKc8vylayqlbKTFGysqjVUSoqzSvJBKk2M6gFAPS5LSY5AAAA&d=H4sIAAAAAAAAAONiDggNEZLKKCkpsNLXz" +
+                "8lPTszJyC8usTI1MDDQL87IL5fiAMnpG-oZKkmWpFaU6BfkJGbmWSskZyQWFaeW2JaWpOlaaFgaKXDxBqUWeqQmpqQW-SXmpgrx" +
+                "A7muFYm5BTmpYYk5pakWjA4MHicZg9idi1ITS1JTEnyzFLh4nPPzSlLzSnRDKgtShQQSCwpyMpMTSzLz8_SzivPzssCmFqOaWox" +
+                "sagFjhW8TIx73AwC26Pni4QAAAA==",
+            urlCaptor.value)
+    }
+
+    @Test
     fun testDeleteDefault() {
         val (client, urlCaptor) = createClientArgCaptor()
 
@@ -153,6 +214,32 @@ class OkLogInterceptorTest {
             "http://responseecho-simonpercic.rhcloud.com/v1/r/H4sIAAAAAAAAAKtWykxRsjLVUcpLzE1VslIKKSpNVXBJLUlNLsksS1" +
                 "XSUSoqzSvJBEmZGQAVpZaU5xdlK1lVg7VZwLV5OPkr1dYCAB5WJYFNAAAA?d=H4sIAAAAAAAAAONic3H1cQ1xFZLJKCkpsNLXz8" +
                 "lPTszJyC8usTI1MDDQL87IL9c31WCwYHJg8DjBGMTk753gW8BY4QsAuiJp6DkAAAA=",
+            urlCaptor.value)
+    }
+
+    @Test
+    fun testDeleteAll() {
+        val (client, urlCaptor) = createClientArgCaptor { it.withAllLogData() }
+
+        val url = server.url("/show/5")
+
+        val request = request(url)
+            .delete(null)
+            .addHeader(REQUEST_HEADER_NAME, REQUEST_HEADER_VALUE)
+            .build()
+
+        val mockResponse = MockResponse()
+            .setBody("{\"id\":5,\"name\":\"True Detective\",\"runtime\":60,\"network\":{\"id\":8,\"name\":\"HBO\"}}")
+            .addHeader(CONTENT_TYPE, APPLICATION_JSON)
+            .addHeader(RESPONSE_HEADER_NAME, RESPONSE_HEADER_VALUE)
+
+        newCall(client, request, mockResponse)
+
+        assertEquals(
+            "http://responseecho-simonpercic.rhcloud.com/v1/r/H4sIAAAAAAAAAKtWykxRsjLVUcpLzE1VslIKKSpNVXBJLUlNLsksS1" +
+                "XSUSoqzSvJBEmZGQAVpZaU5xdlK1lVg7VZwLV5OPkr1dYCAB5WJYFNAAAA?d=H4sIAAAAAAAAAONic3H1cQ1xFZLJKCkpsNLXz8" +
+                "lPTszJyC8usTI1MDDQL87IL9c3leIAyeob6hlqMBgpcPEGpRZ6pCampBb5JeamCvEDua4VibkFOalhiTmlqRZMDgweJxiDmPy9E" +
+                "3yzFLh4nPPzSlLzSnRDKgtShQQSCwpyMpMTSzLz8_SzivPzssAGFqMaWIxsYAFjhW8TI14XAgBwEMrmyAAAAA==",
             urlCaptor.value)
     }
 
@@ -176,6 +263,37 @@ class OkLogInterceptorTest {
         assertEquals(
             "http://responseecho-simonpercic.rhcloud.com/v1/r/0?d=H4sIAAAAAAAAAONi8XB1dBGSzigpKbDS18_JT07MycgvLrEyNT" +
                 "Aw0C_OyC8v1mCwYHJg8JjCHMTpl1-i4JZfmpeScI65gKmCAQAvsqwcPgAAAA==",
+            urlCaptor.value)
+    }
+
+    @Test
+    fun testHeaderAll() {
+        val (client, urlCaptor) = createClientArgCaptor { it.withAllLogData() }
+
+        val url = server.url("/shows")
+
+        val request = request(url)
+            .head()
+            .addHeader(REQUEST_HEADER_NAME, REQUEST_HEADER_VALUE)
+            .build()
+
+        val mockResponse = MockResponse()
+            .setBody("[{\"id\":1,\"name\":\"Under the Dome\",\"runtime\":60,\"network\":{\"id\":2,\"name\":\"CBS\"" +
+                "}},{\"id\":2,\"name\":\"Person of Interest\",\"runtime\":60,\"network\":{\"id\":2,\"name\":\"CBS\"" +
+                "}},{\"id\":4,\"name\":\"Arrow\",\"runtime\":60,\"network\":{\"id\":5,\"name\":\"The CW\"}},{\"id\"" +
+                ":5,\"name\":\"True Detective\",\"runtime\":60,\"network\":{\"id\":8,\"name\":\"HBO\"}},{\"id\"" +
+                ":6,\"name\":\"The 100\",\"runtime\":60,\"network\":{\"id\":5,\"name\":\"The CW\"}},{\"id\"" +
+                ":7,\"name\":\"Homeland\",\"runtime\":60,\"network\":{\"id\":9,\"name\":\"Showtime\"}}]")
+            .setStatus("HTTP/1.1 %d %s".format(404, "Not Found"))
+            .addHeader(CONTENT_TYPE, APPLICATION_JSON)
+            .addHeader(RESPONSE_HEADER_NAME, RESPONSE_HEADER_VALUE)
+
+        newCall(client, request, mockResponse)
+
+        assertEquals(
+            "http://responseecho-simonpercic.rhcloud.com/v1/r/0?d=H4sIAAAAAAAAAONi8XB1dBGSzigpKbDS18_JT07MycgvLrEyNT" +
+                "Aw0C_OyC8vluIASeob6hlqMBgpcPEGpRZ6pCampBb5JeamCvEDua4VibkFOalhiTmlqRZMDgweU5iDOP3ySxTc8kvzUhLOMWcpc" +
+                "PE45-eVpOaV6IZUFqQKCSQWFORkJieWZObn6WcV5-dlgQ0uRjW4GNngAqYKhiZGfA4FAIN8dQDMAAAA",
             urlCaptor.value)
     }
 

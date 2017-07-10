81278e8407dbd119e388ad0467e1cf9851fc7e32
Simon Perčič <simon@percic.si>
2017-07-10 23:26:16 +0200

    CircleCI using API 21 for integration tests

--- config/circleci/run-tests.sh
+++ config/circleci/run-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-emulator -avd circleci-android19 -no-audio -no-window &
+emulator -avd circleci-android21 -no-audio -no-window &
 circle-android wait-for-boot
 ./gradlew check connectedCheck
